2e387242f2e109b6f07228a321822fd63f1089d4
Nikolay <nikmastera@gmail.com>
2018-07-05 11:51:50 +0300

    MiniORM

---
+++ Database Advanced - Entity Framework/02.ORM-Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System.IO;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var connectionString = File.ReadAllText(@"F:\base\MSSQL_connectionString.txt");
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "King",
+                LastName = "Zerkan",
+                IsEmployed = true
+            });
+
+            context.Projects.Add(new Project
+            {
+                Name = "JavaScript Project"
+            });
+            
+            //context.EmployeesProjects.Add(new EmployeeProject
+            //{
+            //    ProjectId = context.Projects.Last().Id,
+            //    EmployeeId = context.Employees.Last().Id
+            //});
+
+            var employee = context.Employees.Last();
+            employee.DepartmentId = context.Departments.First().Id;
+
+            context.SaveChanges();
+        }
+    }
+}
